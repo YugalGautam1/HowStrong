50b0af5379cbb866d1b67c47b02becc31c8e9e98
YugalGautam1 <reachyugal@gmail.com>
2024-12-27 01:28:54 -0500

    Somewhat done

--- app.py
+++ app.py
@@ -7,10 +7,35 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-with open('openpowerlifting-2024-12-14-deb82140.csv', 'r',encoding='utf-8') as file:
+
+with open('openpowerlifting-2024-12-21-6322a115.csv', 'r',encoding='utf-8') as file:
     csv_reader = csv.reader(file)
     data_table = [row for row in csv_reader]  
 
+male = []
+male_tested = []
+female = []
+female_tested = []
+
+for i in range(0,len(data_table)):
+    if(data_table[i][1]=='M'):
+        p = data_table[i][25]
+        if(p.strip()):
+            male+=[(float(data_table[i][25]))]
+            if( data_table[i][31].strip()=="Yes"):
+                male_tested+=[(float(data_table[i][25]))]
+           
+    elif(data_table[i][1]=='F'):
+        p = data_table[i][25]
+        if(p.strip()):
+            female+=[(float(data_table[i][25]))]
+            if( data_table[i][31].strip()=="Yes"):
+                female_tested+=[(float(data_table[i][25]))]
+           
+male = sorted(male)
+female = sorted(female)
+male_tested = sorted(male_tested)
+female_tested=sorted(female_tested)
 
 
 @app.route('/submit', methods=['POST'])
@@ -18,9 +43,12 @@ def submit():
     data = request.json
     lbs = data.get('lbs')
     tested = data.get('tested')
+    gender = data.get('gender')
     total=0
     multiplier=1
     unit = "kgs"
+    
+    
     if(lbs):
         multiplier=2.20462
         unit = "lbs"
@@ -42,28 +70,105 @@ def submit():
     else:
         deadlift = None
     
-
+    
     if(bench and squat and deadlift):
-        if(tested):
-            x=0
-            y = 0
-            for i in range(1,len(data_table)):
-                if(data_table[i][25].strip()!='' and data_table[i][31].strip()=="Yes"):
-                    if(float(data_table[i][25])<total):
-                        x+=1  
-                    y+=1
-            response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(x/y*100,3)}% of Tested Lifters "
+        if(gender=='Male'):
+            if(tested):
+                percentile = 0 
+                if(total>male_tested[-1]):
+                    percentile = 100
+                elif(total<male_tested[0]):
+                    percentile = 0 
+                else: 
+                    low = 0
+                    high = len(male_tested)-2
+                    while(True):
+                        mid = (low+high)//2
+                        if(male_tested[mid]>total):
+                            high=mid-1
+                        elif(male_tested[mid]<total and male_tested[mid+1]<total):
+                            low=mid+1
+                        elif(male_tested[mid]<total and male_tested[mid+1]>=total):
+                            percentile= ((mid+1)/len(male_tested))*100
+                            break
+                        elif(male_tested[mid]==total and male_tested[mid+1]==total):
+                            high=mid-1
 
-            return response
+
+                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of Male Tested Lifters "
+
+                return response
+            else:
+                percentile = 0 
+                if(total>male[-1]):
+                    percentile = 100
+                elif(total<male[0]):
+                    percentile = 0 
+                else: 
+                    low = 1
+                    high = len(male)-2
+                    while(True):
+                        mid = (low+high)//2
+                        if(male[mid]>total):
+                            high=mid-1
+                        elif(male[mid]<total and male[mid+1]<total):
+                            low=mid+1
+                        elif(male[mid]<total and male[mid+1]>=total):
+                            percentile= ((mid+1)/len(male))*100
+                            break
+                        elif(male[mid]==total and male[mid+1]==total):
+                            high=mid-1
+
+                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of all Male Lifters"
         else:
-            x=0
-            y = 0
-            for i in range(1,len(data_table)):
-                if(data_table[i][25].strip()!=''):
-                    if(float(data_table[i][25])<total):
-                        x+=1  
-                    y+=1
-            response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(x/y*100,3)}% of All Lifters"
+            if(tested):
+                percentile = 0 
+                if(total>female_tested[-1]):
+                    percentile = 100
+                elif(total<female_tested[0]):
+                    percentile = 0 
+                else: 
+                    low = 0
+                    high = len(female_tested)-2
+                    while(True):
+                        mid = (low+high)//2
+                        if(female_tested[mid]>total):
+                            high=mid-1
+                        elif(female_tested[mid]<total and female_tested[mid+1]<total):
+                            low=mid+1
+                        elif(female_tested[mid]<total and female_tested[mid+1]>=total):
+                            percentile= ((mid+1)/len(female_tested))*100
+                            break
+                        elif(female_tested[mid]==total and female_tested[mid+1]==total):
+                            high=mid-1
+
+
+                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of Female Tested Lifters "
+
+                return response
+            else:
+                percentile = 0 
+                if(total>female[-1]):
+                    percentile = 100
+                elif(total<female[0]):
+                    percentile = 0 
+                else: 
+                    low = 1
+                    high = len(female)-2
+                    while(True):
+                        mid = (low+high)//2
+                        if(female[mid]>total):
+                            high=mid-1
+                        elif(female[mid]<total and female[mid+1]<total):
+                            low=mid+1
+                        elif(female[mid]<total and female[mid+1]>=total):
+                            percentile= ((mid+1)/len(female))*100
+                            break
+                        elif(female[mid]==total and female[mid+1]==total):
+                            high=mid-1
+
+                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of all Female Lifters"
+        
 
             return response
     return("missing vals")
