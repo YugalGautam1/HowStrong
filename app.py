87113930ace01bdaf6f2e4f76800376cdd4f36bc
Yugal Gautam <yugalgautam@Yugarikas-MacBook-Pro-2.local>
2025-02-03 16:48:18 -0500

    Divisions Added

--- app.py
+++ app.py
@@ -11,185 +11,95 @@ import base64
 
 app = Flask(__name__)
 CORS(app)
+data = []
 
-
-with open('powerlifting-training.csv', 'r',encoding='utf-8') as file:
+with open('data/cleandata.csv', 'r', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
-    data_table = [row for row in csv_reader]  
-
-male = []
-male_tested = []
-female = []
-female_tested = []
-global Selection
-global position 
-Selection = None
-position = None
-
-
-for i in range(0,len(data_table)):
-    if(data_table[i][1]=='M'):
-        p = data_table[i][25]
-        if(p.strip()):
-            male+=[(float(data_table[i][25]))]
-            if( data_table[i][31].strip()=="Yes"):
-                male_tested+=[(float(data_table[i][25]))]
-           
-    elif(data_table[i][1]=='F'):
-        p = data_table[i][25]
-        if(p.strip()):
-            female+=[(float(data_table[i][25]))]
-            if( data_table[i][31].strip()=="Yes"):
-                female_tested+=[(float(data_table[i][25]))]
-           
-male = sorted(male)
-female = sorted(female)
-male_tested = sorted(male_tested)
-female_tested=sorted(female_tested)
+    data_table = [row for row in csv_reader]
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    global Selection
     global position
+    global value
     data = request.json
     lbs = data.get('lbs')
     tested = data.get('tested')
     gender = data.get('gender')
+    weightclass = data.get('weight')
     total=0
     multiplier=1
     unit = "kgs"
-    
     
     if(lbs):
         multiplier=2.20462
         unit = "lbs"
     if(data.get('bench','').strip()):
-        bench = int(data.get('bench', 0))/multiplier
+        bench = float(data.get('bench', 0))/multiplier
         total+=bench
     else:
         bench = None
 
     if(data.get('squat','').strip()):
-        squat = int(data.get('squat', 0))/multiplier
+        squat = float(data.get('squat', 0))/multiplier
         total+=squat
     else:
         squat = None
     
     if(data.get('deadlift','').strip()):
-        deadlift = int(data.get('deadlift', 0))/multiplier
+        deadlift = float(data.get('deadlift', 0))/multiplier
         total+=deadlift
     else:
         deadlift = None
     
     
     if(bench and squat and deadlift):
-        if(gender=='Male'):
+        test = ""
+        if(gender == 'Male'):
+            value = 0 
             if(tested):
-                percentile = 0 
-                if(total>male_tested[-1]):
-                    percentile = 100
-                elif(total<male_tested[0]):
-                    percentile = 0 
-                else: 
-                    low = 0
-                    high = len(male_tested)-2
-                    while(True):
-                        mid = (low+high)//2
-                        if(male_tested[mid]>total):
-                            high=mid-1
-                        elif(male_tested[mid]<total and male_tested[mid+1]<total):
-                            low=mid+1
-                        elif(male_tested[mid]<total and male_tested[mid+1]>=total):
-                            percentile= ((mid+1)/len(male_tested))*100
-                            break
-                        elif(male_tested[mid]==total and male_tested[mid+1]==total):
-                            high=mid-1
-
-                Selection = "Male Tested"
-                position = total
-                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of Male Tested Lifters "
-
-                return response
-            else:
-                percentile = 0 
-                if(total>male[-1]):
-                    percentile = 100
-                elif(total<male[0]):
-                    percentile = 0 
-                else: 
-                    low = 1
-                    high = len(male)-2
-                    while(True):
-                        mid = (low+high)//2
-                        if(male[mid]>total):
-                            high=mid-1
-                        elif(male[mid]<total and male[mid+1]<total):
-                            low=mid+1
-                        elif(male[mid]<total and male[mid+1]>=total):
-                            percentile= ((mid+1)/len(male))*100
-                            break
-                        elif(male[mid]==total and male[mid+1]==total):
-                            high=mid-1
-                Selection = "Male Untested"
-                position = total
-
-                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of all Male Lifters"
-                return response
+                value=13
+                test = " tested"
         else:
+            value = 26
             if(tested):
-                percentile = 0 
-                if(total>female_tested[-1]):
-                    percentile = 100
-                elif(total<female_tested[0]):
-                    percentile = 0 
-                else: 
-                    low = 0
-                    high = len(female_tested)-2
-                    while(True):
-                        mid = (low+high)//2
-                        if(female_tested[mid]>total):
-                            high=mid-1
-                        elif(female_tested[mid]<total and female_tested[mid+1]<total):
-                            low=mid+1
-                        elif(female_tested[mid]<total and female_tested[mid+1]>=total):
-                            percentile= ((mid+1)/len(female_tested))*100
-                            break
-                        elif(female_tested[mid]==total and female_tested[mid+1]==total):
-                            high=mid-1
-
-                Selection = "Female Tested"
-                position = total
-                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of Female Tested Lifters "
-
-                return response
-            else:
-                percentile = 0 
-                if(total>female[-1]):
-                    percentile = 100
-                elif(total<female[0]):
-                    percentile = 0 
-                else: 
-                    low = 1
-                    high = len(female)-2
-                    while(True):
-                        mid = (low+high)//2
-                        if(female[mid]>total):
-                            high=mid-1
-                        elif(female[mid]<total and female[mid+1]<total):
-                            low=mid+1
-                        elif(female[mid]<total and female[mid+1]>=total):
-                            percentile= ((mid+1)/len(female))*100
-                            break
-                        elif(female[mid]==total and female[mid+1]==total):
-                            high=mid-1
-                Selection = "Female Unteested"
-                position = total
-                response = f"Your total lift is {round(total*multiplier,3)} {unit}! This makes you stronger than {round(percentile,3)}% of all Female Lifters"
+                value = 39
+                test = " tested"
+        value+=int(weightclass)
         
+        percentile = 0 
+        total = bench+squat+deadlift
+        position = total
+        high = len(data_table[value])-1
+        low = 1
+        if(total<float(data_table[value][low])):
+            percentile = 0 
+        elif(total>float(data_table[value][high])):
+            percentile = 100
+        else:
+            while(low<=high):
+                mid = (low+high)//2
 
-            return response
-    Selection = None
+
+                if(float(data_table[value][mid])>total):
+                   high = mid-1
+                elif(float(data_table[value][mid])<total and float(data_table[value][mid+1])<total):
+                    low=mid+1
+                elif(float(data_table[value][mid])<total and float(data_table[value][mid+1])>=total):
+                    percentile = ((mid+1)/(len(data_table[value])+1))*100    
+                    break
+                elif(float(data_table[value][mid])==total and float(data_table[value][mid+1])==total):
+                    high = mid-1
+
+        temporary = data_table[value][0][1:]
+        if(temporary[-1]=='T'):
+            temporary=temporary[:-1]
+        temporary = float(temporary)*multiplier
+        
+        response = f"Your total lift is {round(total*multiplier,3)} {unit} This makes you stronger than {round(percentile,3)}% of {test} {gender} Lifters at {round(temporary,1)} {unit}"    
+        return response
+
+    
     position = None
     return("missing vals")
     
@@ -197,28 +107,19 @@ def submit():
 
 @app.route('/histogram', methods=['GET'])
 def line_histogram():
-    global Selection
+    
+    global value
     global position
 
-    if not Selection:
+    if not position:
         return ""
 
     try:
         threshold = position
-        data = []
-        pick = Selection.split()
+        data = data_table[value][1:]
 
-        if len(pick) == 2:
-            if pick[0][0] == 'M':
-                data = male_tested
-            else:
-                data = female_tested
-        else:
-            if pick[0][0] == 'M':
-                data = male
-            else:
-                data = female
-
+        for i in range(0,len(data)):
+            data[i] = float(data[i])
         data = np.array(data)
         
         counts, bin_edges = np.histogram(data, bins="auto")
@@ -265,8 +166,6 @@ def line_histogram():
 
     except Exception as e:
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,162 @@
+import csv
+
+with open('data/openpowerlifting-2024-12-14-deb82140.csv', 'r', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    data_table = [row for row in csv_reader]  
+
+cleandata = []
+for i in range(1,len(data_table)):
+    if(data_table[i][2]=='SBD'):
+        p = data_table[i][25]
+        weight = data_table[i][8]
+        if(p.strip() and weight.strip()):
+            
+            weight = float(data_table[i][8])
+            if(weight <= 44):
+                weight = 44
+            elif(weight <= 48):
+                weight = 48
+            elif(weight <= 52):
+                weight = 52
+            elif(weight <= 56):
+                weight = 56
+            elif(weight <= 60):
+                weight = 60
+            elif(weight <= 67.5):
+                weight = 67.5
+            elif(weight <= 75):
+                weight = 75
+            elif(weight <= 82.5):
+                weight = 82.5
+            elif(weight <= 90):
+                weight = 90
+            elif(weight <= 100):
+                weight = 100
+            elif(weight <= 110):
+                weight = 110
+            elif(weight <= 125):
+                weight = 125
+            else:
+                weight = 140
+            cleandata += [[float(p), data_table[i][1],weight,data_table[i][31]]]
+
+cleandata = sorted(cleandata, key=lambda x: x[0])
+finaldata = [["M44"],["M48"],["M52"],["M56"],["M60"],["M67.5"],["M75"],["M82.5"],["M90"],["M100"],["M110"],["M125"],["M140"],
+             ["M44T"],["M48T"],["M52T"],["M56T"],["M60T"],["M67.5T"],["M75T"],["M82.5T"],["M90T"],["M100T"],["M110T"],["M125T"],["M140T"],
+            ["F44"],["F48"],["F52"],["F56"],["F60"],["F67.5"],["F75"],["F82.5"],["F90"],["F100"],["F110"],["F125"],["F140"],
+             ["F44T"],["F48T"],["F52T"],["F56T"],["F60T"],["F67.5T"],["F75T"],["F82.5T"],["F90T"],["F100T"],["F110T"],["F125T"],["F140T"]
+             ]
+
+for i in range(len(cleandata)):
+    if cleandata[i][1] == 'M':
+        if cleandata[i][2] == 44:
+            finaldata[0] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[13] += [cleandata[i][0]]
+        elif cleandata[i][2] == 48:
+            finaldata[1] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[14] += [cleandata[i][0]]
+        elif cleandata[i][2] == 52:
+            finaldata[2] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[15] += [cleandata[i][0]]
+        elif cleandata[i][2] == 56:
+            finaldata[3] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[16] += [cleandata[i][0]]
+        elif cleandata[i][2] == 60:
+            finaldata[4] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[17] += [cleandata[i][0]]
+        elif cleandata[i][2] == 67.5:
+            finaldata[5] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[18] += [cleandata[i][0]]
+        elif cleandata[i][2] == 75:
+            finaldata[6] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[19] += [cleandata[i][0]]
+        elif cleandata[i][2] == 82.5:
+            finaldata[7] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[20] += [cleandata[i][0]]
+        elif cleandata[i][2] == 90:
+            finaldata[8] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[21] += [cleandata[i][0]]
+        elif cleandata[i][2] == 100:
+            finaldata[9] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[22] += [cleandata[i][0]]
+        elif cleandata[i][2] == 110:
+            finaldata[10] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[23] += [cleandata[i][0]]
+        elif cleandata[i][2] == 125:
+            finaldata[11] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[24] += [cleandata[i][0]]
+        elif cleandata[i][2] == 140:
+            finaldata[12] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[25] += [cleandata[i][0]]
+
+    elif cleandata[i][1] == 'F':
+        if cleandata[i][2] == 44:
+            finaldata[26] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[39] += [cleandata[i][0]]
+        elif cleandata[i][2] == 48:
+            finaldata[27] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[40] += [cleandata[i][0]]
+        elif cleandata[i][2] == 52:
+            finaldata[28] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[41] += [cleandata[i][0]]
+        elif cleandata[i][2] == 56:
+            finaldata[29] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[42] += [cleandata[i][0]]
+        elif cleandata[i][2] == 60:
+            finaldata[30] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[43] += [cleandata[i][0]]
+        elif cleandata[i][2] == 67.5:
+            finaldata[31] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[44] += [cleandata[i][0]]
+        elif cleandata[i][2] == 75:
+            finaldata[32] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[45] += [cleandata[i][0]]
+        elif cleandata[i][2] == 82.5:
+            finaldata[33] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[46] += [cleandata[i][0]]
+        elif cleandata[i][2] == 90:
+            finaldata[34] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[47] += [cleandata[i][0]]
+        elif cleandata[i][2] == 100:
+            finaldata[35] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[48] += [cleandata[i][0]]
+        elif cleandata[i][2] == 110:
+            finaldata[36] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[49] += [cleandata[i][0]]
+        elif cleandata[i][2] == 125:
+            finaldata[37] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[50] += [cleandata[i][0]]
+        elif cleandata[i][2] == 140:
+            finaldata[38] += [cleandata[i][0]]
+            if cleandata[i][3] == 'Yes':
+                finaldata[51] += [cleandata[i][0]]
+
+with open("data/cleandata.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(finaldata)  
+print("Done")
